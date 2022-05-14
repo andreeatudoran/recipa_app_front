f0c58ba0c1a8edc202e6e1b3373147aa32f8476f
Andreea <tudoranandreea18@gmail.com>
2022-05-14 15:24:20 +0300

    Initial setup

---
+++ src/javascript/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header'
+
+function MainPage() {
+    return ( 
+    <div id="MainPage">
+        <Header title={'Recipe app'} />
+    </div>
+    );
+}
+
+export default MainPage;
